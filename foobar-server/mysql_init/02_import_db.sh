14194250f177bf0f7fc4f4e1e6b105dfabb41c6c
IYODA Atsushi <iyoda@atikoro.net>
2017-04-10 20:48:58 +0900

    add

--- foobar-server/mysql_init/02_import_db.sh
+++ foobar-server/mysql_init/02_import_db.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 cd /docker-entrypoint-initdb.d/
 echo "import fuga..."
-gzip -dc ./fuga.dump.gz | mysql --socket=/var/run/mysqld/mysqld.sock -u root -ppassword fuga
+gzip -dc ./fuga.dump.gz | mysql --socket=/tmp/mysql.sock -u root -ppassword fuga
 
 echo "import fuga2..."
-gzip -dc ./fuga2.dump.gz | mysql --socket=/var/run/mysqld/mysqld.sock -u root -ppassword fuga2
+gzip -dc ./fuga2.dump.gz | mysql --socket=/tmp/mysql.sock -u root -ppassword fuga2
 
 echo "done."
